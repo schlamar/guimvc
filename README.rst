292571abb5c1fd31d7be31d31f07e2ff91a84e54
ms4py <marc.schlaich@googlemail.com>
2011-10-06 22:56:00 +0200

    first commit

---
+++ README.rst
@@ -0,0 +1,6 @@
+
+guimvc - Python MVC Framework
+=============================
+
+A lightweight MVC Framework in Python for easy and fast GUI development.
+
---
+++ guimvc/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+'''
+    guimvc
+    ~~~~~~
+
+    A lightweight MVC Framework in Python for easy
+    and fast GUI development.
+
+    :copyright: (c) 2011 by Marc Schlaich
+    :license: MIT, see LICENSE for more details.
+'''
