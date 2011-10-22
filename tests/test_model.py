f772bbc5b8c5befc030af922d99259feec5e39cc
ms4py <marc.schlaich@gmail.com>
2011-10-22 13:58:12 +0200

    wildcard test

--- tests/test_model.py
+++ tests/test_model.py
@@ -7,6 +7,7 @@
     :license: MIT, see LICENSE for more details.
 '''
 
+
 import unittest
 
 from mock import Mock
@@ -17,6 +18,14 @@ from guimvc import Model, Observer
 class TestModel(Model):
     attr1 = 0
     attr2 = 0
+
+
+class WildcardModel(TestModel):
+    __observe__ = ('attr?', 'test*', 'data1')
+    __exclude__ = ('attr2',)
+
+    test_attr = 0
+    data1 = 0
 
 
 class ModelTest(unittest.TestCase):
@@ -41,4 +50,22 @@ class ModelTest(unittest.TestCase):
         obs.notify.assert_called_with('attr1', 1, 0)  # name, new, old
 
         model.attr1 = 1
-        obs.notify.assert_called_with('attr1', 1, 1)  # name, new, old
+        obs.notify.assert_called_with('attr1', 1, 1)
+
+    def test_wildcards(self):
+        model = WildcardModel()
+        obs = Observer(model)
+        obs.notify = Mock()
+
+        model.attr1 = 1
+        obs.notify.assert_called_with('attr1', 1, 0)
+
+        obs.notify.reset_mock()
+        model.attr2 = 2
+        self.assertFalse(obs.notify.called)
+
+        model.test_attr = 1
+        obs.notify.assert_called_with('test_attr', 1, 0)
+
+        model.data1 = 1
+        obs.notify.assert_called_with('data1', 1, 0)
