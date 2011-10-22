467570fa36a7c1cb209cca10dd06acd93d0c14b7
ms4py <marc.schlaich@gmail.com>
2011-10-22 12:44:17 +0200

    pep-8 and nosetest.collector

--- setup.py
+++ setup.py
@@ -3,26 +3,29 @@
 from setuptools import setup
 
 setup(
-      name = 'guimvc',
-      version = '0.1a.dev',
+      name='guimvc',
+      version='0.1a.dev',
       license='MIT',
 
       author='Marc Schlaich',
       author_email='marc.schlaich@gmail.com',
       url='http://guimvc.readthedocs.org/',
-      download_url='http://github.com/ms4py/guimvc/tarball/master#egg=guimvc-dev',
+      download_url=('http://github.com/ms4py/guimvc/tarball/master'
+                    '#egg=guimvc-dev'),
 
-      platforms = 'any',
-      packages = ['guimvc'],
+      platforms='any',
+      packages=['guimvc'],
+      test_suite='nose.collector',
 
       description=('A lightweight MVC Framework in Python for '
                    'easy and fast GUI development.'),
 
-      classifiers=['Development Status :: 3 - Alpha',
-                   'Intended Audience :: Developers',
-                   'License :: OSI Approved :: MIT License',
-                   'Operating System :: OS Independent',
-                   'Programming Language :: Python',
-                   'Topic :: Software Development :: Libraries :: Application Frameworks',
-                   'Topic :: Software Development :: Libraries :: Python Modules'],
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules'],
       )
